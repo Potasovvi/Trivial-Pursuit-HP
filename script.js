1b2f5c72937f5425ea4446b13e32dad44d421281
Potasovvi <157305191+Potasovvi@users.noreply.github.com>
2024-07-23 23:22:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-// Funzione per calcolare il punteggio del quiz
 function calculateScore() {
-    // Definisci le risposte corrette per il quiz corrente
     const answers = {
-        // Risposte corrette per il quiz del secondo film
-        // q1: 'b', q2: 'a', ...
-        // Risposte corrette per il quiz del quinto film
-        // q1: 'a', q2: 'b', ...
+        q1: 'a',
+        q2: 'a',
+        q3: 'b',
+        q4: 'a',
+        q5: 'b',
+        q6: 'b',
+        q7: 'b',
+        q8: 'a',
+        q9: 'a',
+        q10: 'a'
     };
 
     let score = 0;
     const form = document.getElementById('quizForm');
+
     for (let question in answers) {
         const selectedOption = form.querySelector(`input[name=${question}]:checked`);
         if (selectedOption && selectedOption.value === answers[question]) {
