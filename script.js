5a93f8b6f37fce56fc7e9042c2dfdd4c4a902272
Potasovvi <157305191+Potasovvi@users.noreply.github.com>
2024-07-24 19:58:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ function calculateScore() {
 
     document.getElementById('result').innerHTML = `Hai ottenuto ${score} su ${Object.keys(answers).length} punti.`;
 }
+
+
+const correctAnswers = {
+            1: "Dolores Umbridge",
+            2: "Harry Potter"
+        };
+
+        function checkAnswer(questionNumber) {
+            const userAnswer = document.getElementById(`answer${questionNumber}`).value.trim();
+            const feedbackElement = document.getElementById(`feedback${questionNumber}`);
+            
+            if (userAnswer.toLowerCase() === correctAnswers[questionNumber].toLowerCase()) {
+                feedbackElement.textContent = "Risposta corretta!";
+                feedbackElement.style.color = "green";
+            } else {
+                feedbackElement.textContent = "Risposta sbagliata. Riprova.";
+                feedbackElement.style.color = "red";
+            }
+        }
+
+
