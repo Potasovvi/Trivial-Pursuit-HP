c127be315adb066fe2fea12123f92f09bb54e4bc
Potasovvi <157305191+Potasovvi@users.noreply.github.com>
2024-07-24 20:23:31 +0200

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,6 +1,14 @@
 const correctAnswers = {
             1: ["Dolores Umbridge", "Umbridge"],
-            2: ["Harry Potter", "Harry", "Potter"]
+            2: ["Harry Potter", "Harry", "Potter"],
+            3: ["Harry Potter e l'Ordine della Fenice"],
+            4: ["12 Grimmauld Place", "numero 12 di Grimmauld Place"],
+            5: ["Albus Silente"],
+            6: ["Thestral", "Thestrals"],
+            7: ["Cornelius Caramell"],
+            8: ["Cervo"],
+            9: ["Severus Piton", "Piton"],
+            10: ["La Gazzetta del Profeta", "Gazzetta del Profeta"]
         };
 
         function checkAnswer(questionNumber) {
