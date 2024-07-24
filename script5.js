6d8e38298654bcffd69d37389868113e660918f4
Potasovvi <157305191+Potasovvi@users.noreply.github.com>
2024-07-24 20:16:03 +0200

    Update script5.js

--- script5.js
+++ script5.js
@@ -7,14 +7,15 @@ const correctAnswers = {
             const userAnswer = document.getElementById(`answer${questionNumber}`).value.trim().toLowerCase();
             const feedbackElement = document.getElementById(`feedback${questionNumber}`);
             const isCorrect = correctAnswers[questionNumber].some(answer => answer.toLowerCase() === userAnswer);        
-            
-        <--    if (userAnswer.toLowerCase() === correctAnswers[questionNumber].toLowerCase()) { -->
+         
+        <!--    if (userAnswer.toLowerCase() === correctAnswers[questionNumber].toLowerCase()) {  -->  
                         if (isCorrect) {
                 feedbackElement.textContent = "Risposta corretta!";
                 feedbackElement.style.color = "green";
                                        }
-            } else {
+         <!--   } --> 
+                    else {
                 feedbackElement.textContent = "Risposta sbagliata. Riprova.";
                 feedbackElement.style.color = "red";
             }
-        }
+         } 
