7ab88818ca0f97090513a92b1003a5669b690a1c
Potasovvi <157305191+Potasovvi@users.noreply.github.com>
2024-07-24 20:10:59 +0200

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,15 +1,18 @@
 const correctAnswers = {
-            1: "Dolores Umbridge",
-            2: "Harry Potter"
+            1: ["Dolores Umbridge", "Umbridge"],
+            2: ["Harry Potter", "Harry", "Potter"]
         };
 
         function checkAnswer(questionNumber) {
             const userAnswer = document.getElementById(`answer${questionNumber}`).value.trim();
             const feedbackElement = document.getElementById(`feedback${questionNumber}`);
+            const isCorrect = correctAnswers[questionNumber].some(answer => answer.toLowerCase() === userAnswer);        
             
             if (userAnswer.toLowerCase() === correctAnswers[questionNumber].toLowerCase()) {
+                        if (isCorrect) {
                 feedbackElement.textContent = "Risposta corretta!";
                 feedbackElement.style.color = "green";
+                                       }
             } else {
                 feedbackElement.textContent = "Risposta sbagliata. Riprova.";
                 feedbackElement.style.color = "red";
